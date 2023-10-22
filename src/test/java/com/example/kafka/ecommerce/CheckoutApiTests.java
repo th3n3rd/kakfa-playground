c1f72137640946743c58dac7bd89cbc0f25b4ab0
Marco Garofalo <marcogarofalo.personal@gmail.com>
2023-10-22 19:13:13 +0100

    Decouple api test from spring events

--- src/test/java/com/example/kafka/ecommerce/CheckoutApiTests.java
+++ src/test/java/com/example/kafka/ecommerce/CheckoutApiTests.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(CheckoutApi.class)
-@Import({Orders.class, ShoppingCart.class, Checkout.class, SpringEvents.class})
+@Import({Orders.class, ShoppingCart.class, Checkout.class, InMemoryEvents.class})
 class CheckoutApiTests {
 
     @Autowired
