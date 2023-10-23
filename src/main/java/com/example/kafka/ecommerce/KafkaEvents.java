6764a0b35d57ca5b52a9dcc8a6ed4822b7402a45
Marco Garofalo <marcogarofalo.personal@gmail.com>
2023-10-23 10:10:57 +0100

    Leverage kafka for producing and consuming events

---
+++ src/main/java/com/example/kafka/ecommerce/KafkaEvents.java
@@ -0,0 +1,33 @@
+package com.example.kafka.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Profile("kafka-events")
+@Primary
+@Component
+class KafkaEvents implements Events {
+
+    private final KafkaTemplate<String, Object> kafka;
+    private final ApplicationEventPublisher eventPublisher;
+
+    KafkaEvents(KafkaTemplate<String, Object> kafka, ApplicationEventPublisher eventPublisher) {
+        this.kafka = kafka;
+        this.eventPublisher = eventPublisher;
+    }
+
+    @Override
+    public void publish(Object event) {
+        kafka.send(KafkaConfig.Topic, event);
+    }
+
+    @KafkaListener(topics = KafkaConfig.Topic, groupId = "ecommerce")
+    void consume(ConsumerRecord<?, ?> event) {
+        eventPublisher.publishEvent(event.value());
+    }
+}
